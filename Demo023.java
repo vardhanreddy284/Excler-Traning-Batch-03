50285489c55763df6f1c970b3fe49954c9ee7f17
vardhanreddy284 <vardhanreddykalleda@gmail.com>
2025-01-22 22:21:13 +0530

    Is palindrome

---
+++ Demo023.java
@@ -0,0 +1,27 @@
+
+public class Demo023 {
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(232));
+        System.out.println(isPalindrome(345));
+        System.out.println(isPalindrome(-123));
+        System.out.println(isPalindrome(0));
+    }
+
+    public static int isPalindrome(int x) {
+        if (x <= 0) {
+            return -1;
+        }
+        int reversed = 0;
+        int original = x;
+        while (x > 0) {
+            int digit = x % 10;
+            reversed = reversed * 10 + digit;
+            x = x / 10;
+        }
+        if (original == reversed) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+}
