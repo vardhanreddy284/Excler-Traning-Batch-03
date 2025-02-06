39409893168208030670ec7469f6ab6022ddafc8
vardhanreddy284 <vardhanreddykalleda@gmail.com>
2025-02-06 12:11:32 +0530

    the sum of digits

---
+++ Demo048.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Demo048 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        
+        int sum = 0;
+        
+        while (number > 0) {
+            sum += number % 10;
+            number /= 10;
+        }
+        
+        System.out.println("Sum of digits: " + sum);
+        scanner.close();
+}
+}
